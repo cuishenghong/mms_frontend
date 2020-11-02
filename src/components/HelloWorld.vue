b050f4ac37e84da5a3048b694653307a2621ad33
NancyLiu <759686484@qq.com>
2020-11-02 20:54:10 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-container fluid class="p-4">
+    <b-row>
+      <b-col sm="3" v-for="item in list" v-bind:key="item.index">
+        <b-img thumbnail fluid :src="item.strCategoryThumb" v-bind="mainProps"></b-img>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      mainProps: {
+        class: 'mb-2'
+      },
+      list: []
+    }
+  },
+  mounted() {
+    axios
+        .get("https://www.themealdb.com/api/json/v1/1/categories.php")
+        .then(response => {
+        this.list = response.data.categories
+      })
+        .catch(err => {
+        console.log(err)
+      })
+  }
+}
+</script>
